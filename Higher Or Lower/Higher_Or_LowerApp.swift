230d64902ce4e6c91478db3b9b81411394e2e888
mason <mkaydo@bsd220.org>
2025-11-18 10:51:33 -0600

    Initial Commit

---
+++ Higher Or Lower/Higher_Or_LowerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Higher_Or_LowerApp.swift
+//  Higher Or Lower
+//
+//  Created by mason kaydo on 11/18/25.
+//
+
+import SwiftUI
+
+@main
+struct Higher_Or_LowerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
